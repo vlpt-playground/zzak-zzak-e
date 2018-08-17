4bfc0d6877bc263abb8459fb84051e147378afbe
velopert <public.velopert@gmail.com>
2018-08-18 07:30:15 +0900

    fixed cors header issue

--- zze-server/src/lib/cors.js
+++ zze-server/src/lib/cors.js
@@ -1,5 +1,9 @@
 module.exports = (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', '*');
   ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
   return next();
 };
