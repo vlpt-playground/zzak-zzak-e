e05c943361c8b3b6acd691bea032d173b37768e2
Ubuntu <ubuntu@ip-172-31-25-251.ap-northeast-2.compute.internal>
2018-08-24 13:07:09 +0000

    allow cors http only cookie

--- zze-server/src/lib/cors.js
+++ zze-server/src/lib/cors.js
@@ -1,5 +1,6 @@
 module.exports = (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Origin', ctx.headers.origin);
+  ctx.set('Access-Control-Allow-Credentials', true);
   ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   ctx.set(
     'Access-Control-Allow-Headers',
