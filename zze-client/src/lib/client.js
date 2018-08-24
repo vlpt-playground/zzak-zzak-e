92ee164e1717c513f06a49cbad83012ba86ef1e6
velopert <public.velopert@gmail.com>
2018-08-24 22:09:09 +0900

    add withCredentials to client

--- zze-client/src/lib/client.js
+++ zze-client/src/lib/client.js
@@ -5,7 +5,7 @@ const client = axios.create({
     process.env.NODE_ENV === 'development'
       ? '/'
       : 'https://zzakzzak-e.vlpt.us/',
+  withCredentials: true, // 다른 도메인에 httpOnly 쿠키 사용 할 때 필수
 });
-
 
 export default client;
